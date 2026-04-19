3457d62b1bb79f9c3a0c323ee4e7efda04932adc
Vanessa Saavedra <vanessa.saavedraRMZ@gmail.com>
2026-04-18 20:11:49 -0600

    Vanessa Saavedra: añadir peliculas

--- src/data/peliculasLibres.js
+++ src/data/peliculasLibres.js
@@ -1,13 +1,47 @@
 export const peliculasLibres = [
   {
-    id: 27374, 
+    id: 27374,
     title: "Lake Mungo",
-    poster_path: "https://www.themoviedb.org/t/p/w600_and_h900_face/g0zCELYfBfSv8TOGC13buABVN53.jpg", 
-    backdrop_path: "https://www.themoviedb.org/t/p/w600_and_h900_face/g0zCELYfBfSv8TOGC13buABVN53.jpg",
+    poster_path: "/vGvG6vJgRInR7U3vI6v6XvJ6XvJ.jpg", 
     vote_average: 7.0,
-    release_date: "2008",
-    overview: "Tras la muerte de una joven, su familia comienza a experimentar sucesos extraños que sugieren que ella escondía un oscuro secreto.",
-    isFree: true, 
+    release_date: "2008-06-18",
+    isFree: true,
     youtubeId: "GWVGlYBEEXw"
+  },
+  {
+    id: 297907,
+    title: "Pasajeros",
+    poster_path: "/9re6vN48L2O8uM8m98v6q9K9S.jpg", 
+    vote_average: 6.5,
+    release_date: "2008-10-24",
+    isFree: true,
+    youtubeId: "sg4HgAHmRac"
+  },
+  {
+    id: 82507,
+    title: "Siniestro",
+    poster_path: "/Ams77w6u5p6M5Z6N6u8m9V6q.jpg", 
+    vote_average: 7.2,
+    release_date: "2012-10-12",
+    isFree: true,
+    youtubeId: "g5SqFIGb8-0"
+  },
+  {
+    id: 119278,
+    title: "Smiley",
+    poster_path: "/Am7m9v6q9K9S6u8m9V6q9K9S.jpg", 
+    vote_average: 5.0,
+    release_date: "2012-10-11",
+    isFree: true,
+    youtubeId: "vPF-rEKb0LI"
+  },
+  {
+    id: 1427,
+    title: "El Perfume",
+    poster_path: "https://www.themoviedb.org/t/p/w1280/dAR6MYoz8VrUhGsT0F8ZsqONtdp.jpg", 
+    vote_average: 7.8,
+    release_date: "2006-09-13",
+    isFree: true,
+    youtubeId: "-wOt1ucLCW0"
   }
 ];
